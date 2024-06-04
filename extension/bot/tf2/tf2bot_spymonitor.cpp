4c9fc5d972e7cf9b076c1ae1d3fe8cc1f2cc2652
caxanga334 <10157643+caxanga334@users.noreply.github.com>
2024-06-04 14:58:14 -0300

    Add Missed Files

---
+++ extension/bot/tf2/tf2bot_spymonitor.cpp
@@ -0,0 +1,200 @@
+#include <algorithm>
+
+#include <extension.h>
+#include <entities/tf2/tf_entities.h>
+#include <util/helpers.h>
+#include <mods/tf2/tf2lib.h>
+#include "tf2bot.h"
+#include "tf2bot_spymonitor.h"
+
+CTF2BotSpyMonitor::CTF2BotSpyMonitor(CBaseBot* bot) :
+	IBotInterface(bot)
+{
+	m_knownspylist.reserve(100);
+}
+
+CTF2BotSpyMonitor::~CTF2BotSpyMonitor()
+{
+}
+
+void CTF2BotSpyMonitor::Reset()
+{
+	m_knownspylist.clear();
+}
+
+void CTF2BotSpyMonitor::Update()
+{
+	CTF2Bot* me = static_cast<CTF2Bot*>(GetBot());
+	CTF2BotSensor* vision = me->GetSensorInterface();
+	const CKnownEntity* known = nullptr;
+
+	// Remove obsolete spies from the list
+	m_knownspylist.erase(std::remove_if(m_knownspylist.begin(), m_knownspylist.end(), [](const KnownSpy& object) {
+		return object.IsObsolete();
+	}), m_knownspylist.end());
+
+	for (auto& knownspy : m_knownspylist)
+	{
+		knownspy.Update(me);
+	}
+}
+
+void CTF2BotSpyMonitor::Frame()
+{
+}
+
+const CTF2BotSpyMonitor::KnownSpy& CTF2BotSpyMonitor::GetKnownSpy(edict_t* spy)
+{
+	for (auto& known : m_knownspylist)
+	{
+		if (known(spy))
+		{
+			return known;
+		}
+	}
+
+	// not found
+	auto& ret = m_knownspylist.emplace_back(spy);
+	ret.Update(static_cast<CTF2Bot*>(GetBot())); // New spy, update it
+	return ret;
+}
+
+CTF2BotSpyMonitor::KnownSpy::KnownSpy(edict_t* spy) :
+	m_handle(spy)
+{
+	m_detectionLevel = DETECTION_NEW;
+	m_initialDetectionTime.Start();
+}
+
+CTF2BotSpyMonitor::KnownSpy::KnownSpy(int spy)
+{
+	m_handle.Set(gamehelpers->EdictOfIndex(spy));
+	m_detectionLevel = DETECTION_NEW;
+	m_initialDetectionTime.Start();
+}
+
+void CTF2BotSpyMonitor::KnownSpy::Update(CTF2Bot* me)
+{
+	edict_t* spy = m_handle.Get();
+
+	if (spy == nullptr)
+	{
+		return;
+	}
+
+	if (!me->GetSensorInterface()->IsAbleToSee(spy))
+	{
+		if (m_detectionLevel == DETECTION_BLOWN && m_blownTime.IsGreaterThen(LOST_LOS_TIME))
+		{
+			// line of sight was lost, become suspicious
+			m_detectionLevel = DETECTION_SUSPICIOUS;
+		}
+		else if (m_detectionLevel == DETECTION_SUSPICIOUS && m_blownTime.IsLessThen(SUSPICIOUS_TIME))
+		{
+			// Major loss of line of sight
+			m_detectionLevel = DETECTION_NEW;
+		}
+
+		return;
+	}
+
+	// Already detected and visible, maintain detection status
+	if (m_detectionLevel == DETECTION_BLOWN)
+	{
+		OnDetected();
+		return;
+	}
+
+	int client = gamehelpers->IndexOfEdict(spy);
+
+	// Detection: Not disguised
+	if (!tf2lib::IsPlayerInCondition(client, TeamFortress2::TFCond::TFCond_Disguised))
+	{
+		OnDetected();
+		return;
+	}
+
+	// Detection: Recent memory of known spy
+	if (m_detectionLevel == DETECTION_SUSPICIOUS && m_blownTime.IsLessThen(SUSPICIOUS_TIME))
+	{
+		OnDetected();
+		return;
+	}
+
+	edict_t* pTarget = tf2lib::GetDisguiseTarget(client);
+
+	if (pTarget == nullptr)
+	{
+		return;
+	}
+
+	int target = gamehelpers->IndexOfEdict(pTarget);
+
+	// Detection: Disguised as me
+	if (target == me->GetIndex())
+	{
+		OnDetected();
+		return;
+	}
+
+	// Detection: Target is dead
+	if (!UtilHelpers::IsPlayerAlive(target))
+	{
+		OnDetected();
+		return;
+	}
+
+	auto disguiseClass = tf2lib::GetDisguiseClass(client);
+
+	// Detection: Spy disguise class doesn't match the target current class
+	if (tf2lib::GetPlayerClassType(target) != disguiseClass)
+	{
+		OnDetected();
+		return;
+	}
+
+	// Detection: Disguised as a scout while moving
+	if (disguiseClass == TeamFortress2::TFClass_Scout)
+	{
+		tfentities::HTFBaseEntity be(spy);
+		Vector velocity = be.GetAbsVelocity();
+		float speed = velocity.Length();
+
+		if (speed > 100.0f)
+		{
+			OnDetected();
+			return;
+		}
+	}
+
+	// Detection: Medics should be healing
+	// TO-DO: Make this be based on the bot skill
+	if (disguiseClass == TeamFortress2::TFClass_Medic)
+	{
+		OnDetected();
+		return;
+	}
+
+	OnNoticed();
+}
+
+bool CTF2BotSpyMonitor::KnownSpy::operator==(const KnownSpy& other)
+{
+	if (m_handle.Get() == nullptr || other.GetEdict() == nullptr)
+	{
+		return false;
+	}
+
+	return gamehelpers->IndexOfEdict(m_handle.Get()) == gamehelpers->IndexOfEdict(other.GetEdict());
+}
+
+bool CTF2BotSpyMonitor::KnownSpy::operator()(edict_t* spy)
+{
+	if (m_handle.Get() == nullptr)
+	{
+		return false;
+	}
+
+	return gamehelpers->IndexOfEdict(m_handle.Get()) == gamehelpers->IndexOfEdict(spy);
+}
+
---
+++ extension/bot/tf2/tf2bot_spymonitor.h
@@ -0,0 +1,77 @@
+#ifndef NAVBOT_TF2BOT_SPY_MONITOR_INTERFACE_H_
+#define NAVBOT_TF2BOT_SPY_MONITOR_INTERFACE_H_
+#pragma once
+
+#include <vector>
+#include <util/ehandle_edict.h>
+#include <sdkports/sdk_timers.h>
+#include <bot/interfaces/base_interface.h>
+
+class CTF2Bot;
+
+class CTF2BotSpyMonitor : public IBotInterface
+{
+public:
+	CTF2BotSpyMonitor(CBaseBot* bot);
+	~CTF2BotSpyMonitor() override;
+
+	enum SpyDetectionLevel
+	{
+		DETECTION_INVALID = 0,
+		DETECTION_NEW, // A new spy has been spotted
+		DETECTION_FOOLED, // The spy has fooled me
+		DETECTION_SUSPICIOUS, // The spy is acting suspicious, monitor them
+		DETECTION_BLOWN, // Enemy spy has been detected, attack!
+
+		MAX_DETECTION_TYPES
+	};
+
+	/**
+	 * @brief Represents the data the bot knowns about a spy
+	 */
+	class KnownSpy
+	{
+	public:
+		KnownSpy(edict_t* spy);
+		KnownSpy(int spy);
+
+		static constexpr auto FORGET_SPY_TIME = 25.0f;
+		static constexpr auto SUSPICIOUS_TIME = 10.0f;
+		static constexpr auto LOST_LOS_TIME = 2.5f;
+
+		void Update(CTF2Bot* me);
+
+		bool operator==(const KnownSpy& other);
+		bool operator()(edict_t* spy);
+		edict_t* GetEdict() const { return m_handle.Get(); }
+		bool IsObsolete() const { return m_handle.Get() == nullptr || m_detectionLevel == DETECTION_INVALID || (m_forgetTime.HasStarted() && m_forgetTime.IsElapsed()); }
+		void Invalidate() { m_handle.Term(); }
+		bool IsHostile() const { return m_detectionLevel == DETECTION_BLOWN; }
+		SpyDetectionLevel GetDetectionLevel() const { return m_detectionLevel; }
+		void NotifyTouch() { OnDetected(); }
+
+	private:
+		CHandleEdict m_handle;
+		SpyDetectionLevel m_detectionLevel;
+		IntervalTimer m_initialDetectionTime; // Time since the spy has been added to the known database
+		IntervalTimer m_blownTime; // Time since the spy has been detected as an enemy spy
+		CountdownTimer m_forgetTime; // Time to forget about this spy
+
+		void OnDetected() { m_detectionLevel = DETECTION_BLOWN; m_forgetTime.Start(FORGET_SPY_TIME); m_blownTime.Start(); }
+		void OnNoticed() { m_forgetTime.Start(FORGET_SPY_TIME); }
+	};
+
+	// Reset the interface to it's initial state
+	void Reset() override;
+	// Called at intervals
+	void Update() override;
+	// Called every server frame
+	void Frame() override;
+
+	const KnownSpy& GetKnownSpy(edict_t* spy);
+
+private:
+	std::vector<KnownSpy> m_knownspylist;
+};
+
+#endif // !NAVBOT_TF2BOT_SPY_MONITOR_INTERFACE_H_
